19a971ddd294de1a1e7ad291f7bc0da7ac03a17d
Lachlan Todd <lachlan.todd@hotmail.com>
2020-02-23 19:29:52 +1000

    changed game screen to not fade in/out

--- Runway-Game/Assets/Scripts/RestartScript.cs
+++ Runway-Game/Assets/Scripts/RestartScript.cs
@@ -5,27 +5,14 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class RestartScript : MonoBehaviour
-{
-    private int scene;
-    public Animator anim;
-    public Image black;
-
+{ 
     public void RestartLevel()
     {
-        scene = 3;
-        StartCoroutine("Fader");
+        SceneManager.LoadScene(3);
     }
 
     public void ReturnToMenu()
     {
-        scene = 0;
-        StartCoroutine("Fader");
-    }
-
-    private IEnumerator Fader()
-    {
-        anim.SetBool("Fade", true);
-        yield return new WaitUntil(() => black.color.a == 1);
-        SceneManager.LoadScene(scene);
+        SceneManager.LoadScene(0);
     }
 }
